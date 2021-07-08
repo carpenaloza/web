6c1e0a84cb2a0f5005b1ab7e0a7b69cae598dfc3
Carolina Peñaloza <car.penaloza@hotmail.es>
2021-07-08 09:50:01 -0400

    operaciones

---
+++ Operaciones/operaciones.js
@@ -0,0 +1,18 @@
+console.log(pregunta)
+
+//Objeto donde agrupamos datos
+persona = {
+    "nombre": "Marcos",
+    "edad": 88,
+    "peso": 70.4
+}
+
+persona2 = {
+    "nombre": "Camila",
+    "edad": 15,
+    "peso": 45
+}
+
+promedio_edades = (persona.edad + persona2.edad) / 2
+
+console.log(promedio_edades)
