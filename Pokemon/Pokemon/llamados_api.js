4847233600d49d7ef7268f0fcd8ad7fcadeabaa5
carpenaloza <car.penaloza@hotmail.es>
2021-09-07 23:47:24 -0300

    arreglo

--- Pokemon/Pokemon/llamados_api.js
+++ Pokemon/Pokemon/llamados_api.js
@@ -2,7 +2,7 @@ function buscarPokemon(){
 
         nombre_pokemon = $("#txt-busqueda").val()
 
-        fetch("https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon)
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + nombre_pokemon)
         .then(respuesta => respuesta.json())
         .then(datos => {
             nueva_etiqueta = `
@@ -13,14 +13,19 @@ function buscarPokemon(){
 
             $(".card-text").html(`
             <p>
+            Nombre: ${datos.name}
+            <br>
             N° : ${datos.id} 
             <br> 
             Altura: ${datos.height}
             <br>
             Peso: ${datos.weight}
             <br>
+            Experiencia ${datos.base_experience}
+            <br>
             Ataques: ${datos.moves[0].move.name}, ${datos.moves[1].move.name}, ${datos.moves[2].move.name}, ${datos.moves[3].move.name}, ${datos.moves[4].move.name}
             <br>
+            Habilidades: ${datos.abilities[0].ability.name}, ${datos.abilities[1].ability.name}
             </p>
             `)
 
